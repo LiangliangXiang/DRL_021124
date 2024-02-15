9b7c333179afcf83f4ac76b0c4870af9954b9eff
lxia467@aucklanduni.ac.nz <lxia467@UoA.auckland.ac.nz>
2024-02-15 20:46:34 +1300

    add new commit

--- rl.py
+++ rl.py
@@ -11,7 +11,7 @@ model.learn(total_timesteps=10_000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(100000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
